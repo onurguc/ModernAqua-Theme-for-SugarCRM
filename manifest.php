44ca84d77c808ee8fc9b843560de53451e826faf
onurguc <onurguc@yahoo.com>
2013-12-22 16:58:15 +0200

    Update manifest.php

--- manifest.php
+++ manifest.php
@@ -1,7 +1,7 @@
 <?php
 $manifest = array (
 	'acceptable_sugar_versions' => array(
-		'exact_matches' => array(),
+		'exact_matches' => array(6.5.16),
 		'regex_matches' => array(
 			0 => '6\.0\.\d(beta\d|RC\d|[a-z])?',
 			1 => '6\.1\.\d(beta\d|RC\d|[a-z])?',
